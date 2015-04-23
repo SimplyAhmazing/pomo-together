ac60ed84d80fd1b91c929e047d7f1f29d4634662
Ahmed Abdalla <aabdalla@amplify-nation.com>
2015-04-23 17:10:32 -0400

    sets up running server w/singleton clock

---
+++ modules/clock.js
@@ -0,0 +1,35 @@
+var util = require('util');
+var EventEmitter = require('events').EventEmitter;
+
+var pomoTimes = [25, 5, 15];
+var currentCountDown = null;
+var currentIndex = 0;
+
+
+function getCountDown(){
+    var time = pomoTimes[currentIndex % pomoTimes.length];
+    currentIndex += 1;
+    return time;
+}
+
+
+
+function Clock (){};
+util.inherits(Clock, EventEmitter);
+
+Clock.prototype.run = function(){
+    var self = this;
+    setInterval(function(){
+        if (currentCountDown <= 0){
+            currentCountDown = getCountDown();
+        };
+
+        console.log('New server tick. Time: ', currentCountDown);
+        currentCountDown = currentCountDown - (1/60);
+        self.emit('tick', currentCountDown);
+    }, 1000);
+}
+
+var clock = new Clock();
+
+module.exports = clock;
---
+++ modules/pomo.js
@@ -0,0 +1,8 @@
+var clock = require('./clock');
+
+var pomoTimes = [25, 5, 15];
+
+function run(callback){
+  var currentInterval = pomoTimes.shift();
+  clock(time
+}
